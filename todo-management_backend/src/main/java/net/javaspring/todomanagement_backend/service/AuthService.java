9ac9be2d0e7f6420df7cee2833e0afd1e78e144e
Cosmina <recea.cosmina@gmail.com>
2024-04-30 00:39:48 +0300

    Getting the role as response

--- todo-management_backend/src/main/java/net/javaspring/todomanagement_backend/service/AuthService.java
+++ todo-management_backend/src/main/java/net/javaspring/todomanagement_backend/service/AuthService.java
@@ -1,10 +1,11 @@
 package net.javaspring.todomanagement_backend.service;
 
+import net.javaspring.todomanagement_backend.dto.JwtAuthResponse;
 import net.javaspring.todomanagement_backend.dto.LoginDto;
 import net.javaspring.todomanagement_backend.dto.RegisterDto;
 
 public interface AuthService {
     String register(RegisterDto registerDto);
 
-    String login(LoginDto loginDto);
+    JwtAuthResponse login(LoginDto loginDto);
 }
